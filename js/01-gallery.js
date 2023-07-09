eaefce0f7b856e506bf3c456e80de2fefe7ff56d
AnnDribnokhod <88674960+AnnDribnokhod@users.noreply.github.com>
2023-07-09 18:05:31 +0300

    v1-hw

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const getGalleryList = document.querySelector(".gallery");
+
+function createGalleryItems(galleryItemsArray) {
+  return galleryItemsArray
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="#" target= "_self">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join("");
+}
+getGalleryList.insertAdjacentHTML(
+  "beforeend",
+  createGalleryItems(galleryItems)
+);
+getGalleryList.addEventListener("click", handleImageClick);
+
+function handleImageClick(evt) {
+  if (evt.target.classList.contains("gallery__image")) {
+    console.log(evt.target.dataset.source);
+    const { source } = evt.target.dataset;
+    const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+    instance.show();
+  }
+}
